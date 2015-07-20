15a96f144e469f140d5e0c9b76d1daea905c10de
kslisenko <kslisenko@gmail.com>
2015-07-20 08:53:10 +0300

    test strategies

--- jpa-se-examples/src/test/java/net/slisenko/jpa/examples/caching/TestCachingStrategies.java
+++ jpa-se-examples/src/test/java/net/slisenko/jpa/examples/caching/TestCachingStrategies.java
@@ -1,6 +1,12 @@
 package net.slisenko.jpa.examples.caching;
 
+import net.slisenko.jpa.examples.caching.model.CachedEntity;
+import net.slisenko.jpa.examples.caching.model.NonStrictReadWriteEntity;
+import net.slisenko.jpa.examples.caching.model.ReadOnlyEntity;
+import org.junit.Assert;
 import org.junit.Test;
+
+import javax.persistence.RollbackException;
 
 ///**
 // * TODO check all this behaviour
@@ -19,16 +25,58 @@ import org.junit.Test;
 // */
 public class TestCachingStrategies extends BaseCacheTest {
 
-     @Test
+     @Test(expected = RollbackException.class)
      public void testReadOnly() {
+          p("Persist initial entity");
           em.getTransaction().begin();
-
-
+          // Save read only entity
+          ReadOnlyEntity re = new ReadOnlyEntity();
+          re.setName("read only initial");
+          em.persist(re);
           em.getTransaction().commit();
+          em.clear();
+
+          p("Change entity name");
+          em.getTransaction().begin();
+          // Read entity, populate cache
+          re = em.find(ReadOnlyEntity.class, re.getId());
+          assertCached(re);
+          // Change entity
+          re.setName("changed");
+          em.getTransaction().commit();
+          em.clear();
+
+          p("Get entity second time");
+          re = em.find(ReadOnlyEntity.class, re.getId());
+          Assert.assertEquals(re.getName(), "changed");
      }
 
      @Test
      public void testReadWrite() {
+          p("========== Persist initial entity ==========");
+          em.getTransaction().begin();
+          // Save read only entity
+          NonStrictReadWriteEntity re = new NonStrictReadWriteEntity();
+          re.setName("read write initial");
+          em.persist(re);
+          em.getTransaction().commit();
+          em.clear();
+
+          p("========== Get entity, use SQL for populating cache ==========");
+          em.getTransaction().begin();
+          // Read entity, populate cache
+          re = em.find(NonStrictReadWriteEntity.class, re.getId());
+          assertCached(re);
+          // Change entity
+          em.getTransaction().commit();
+          em.clear();
+
+          p("========== Get entity second time, no SQL used, change name ==========");
+          em.getTransaction().begin();
+          re = em.find(NonStrictReadWriteEntity.class, re.getId());
+          re.setName("changed");
+          em.getTransaction().commit();
+          em.clear();
 
      }
 
